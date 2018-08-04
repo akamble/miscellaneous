0583cbd00dcbc94558334571ab4331da1f6f2b8a
Abhijeet Kamble <abhijeet.kamble@here.com>
2018-08-05 00:24:20 +0530

    Binary search

---
+++ src/main/java/BinarySearch.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class BinarySearch {
+
+
+    static int  binarySearch(int [] array,int searchKey){
+            int start=0;
+            int end = array.length-1;
+            while (start<=end){
+                int mid = (start+end)/2;
+                if(searchKey==array[mid])
+                    return searchKey;
+                if(searchKey<array[mid])
+                    end = mid -1;
+                else
+                    start = mid +1;
+            }
+return  -1;
+
+    }
+
+    public static void main(String[] args) {
+
+        int array[] = {1,2,6,3,4,9,10,3,67,23,82,23,5};
+        int searchKey = 4;
+        Arrays.sort(array);
+//        Arrays.stream(array).forEach(System.out::print);
+        System.out.println(binarySearch(array,searchKey));
+
+    }
+}
--- src/main/java/collections/PairFinding.java
+++ src/main/java/collections/PairFinding.java
@@ -2,6 +2,28 @@ package  collections;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/*
+*
+* Coding Challenge
+
+     In this coding challenge you will be assessed on your ability to solve problems, the cleanliness of your code, and your testing skills.
+
+The Challenge:
+     Using Java, write a console application that takes a string as a command line parameter, performs the calculation defined below, and writes the output to the console.
+Include the appropriate unit tests.
+
+For example
+     Given the string "gaeb7!!!8jeks5!!!tux10" return true if there are exactly 3 exclamation marks  between every pair of numbers that add up to 15. Otherwise return false.
+ (Note: exclamation marks may be separated by other, non-numeric characters)
+
+See the examples below:
+     “gaeb7!!!8jeks5!!!tux10”   => true
+     “kem!7!!nej!8ww1!!!!!!5”   => true
+     “7!!!8!!!7!!!8!!!7”        => true
+     “5!!aaaaaaaaaaaaa!10!5”    => false
+     “Aa6!9”                    => false
+* */
+
 public class PairFinding {
 
     public static void main(String[] args) {
